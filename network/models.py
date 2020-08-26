38cc6780ba7440db3ff0338635e73e8a01bc73bb
Isabella Enriquez <45607721+riquezen@users.noreply.github.com>
2020-08-26 00:10:57 -0400

    Uploaded complete project

---
+++ network/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+class Post(models.Model):
+    content = models.TextField()
+    likes = models.IntegerField(default=0)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_posts")
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    """def __str__(self):
+        return self.author + " posted: \"" + self.content + "\""
+        """
+
+class Follower(models.Model):
+    username = models.CharField(max_length=150)
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followers") #user being followed
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="likers")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="liked_post")
